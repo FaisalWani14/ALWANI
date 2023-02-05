19a3a7022cbe6150e08e6aa535d93c631cd4b2a3
FaisalWani14 <117328589+FaisalWani14@users.noreply.github.com>
2023-02-05 16:51:43 +0700

    Initial commit

---
+++ src/components/Navbar.js
@@ -0,0 +1,39 @@
+import { Component } from "react";
+import "./NavbarStyles.css";
+import { MenuItems } from "./MenuItems";
+
+class Navbar extends Component {
+  state = { clicked: false };
+  handleClick = () => {
+    this.setState({ clicked: !this.state.clicked });
+  };
+  render() {
+    return (
+      <nav className="NavbarItems">
+        <h1 className="navbar-logo">ALWANI TRAVEL</h1>
+
+        <div className="menu-icons" onClick={this.handleClick}>
+          <i
+            className={this.state.clicked ? "fas fa-times" : "fas fa-bars"}
+          ></i>
+        </div>
+
+        <ul className="nav-menu">
+          {MenuItems.map((item, index) => {
+            return (
+              <li key={index}>
+                <a className={item.cName} href={item.url}>
+                  <i class={item.icon}></i>
+                  {item.title}
+                </a>
+              </li>
+            );
+          })}
+          <button>Sign Up</button>
+        </ul>
+      </nav>
+    );
+  }
+}
+
+export default Navbar;
